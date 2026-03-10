df9c5ec057fec565fe4f2963890652e5e073a028
Jean Paul Ferreira <jeanpaul@jepafe.net>
2026-03-10 16:02:08 -0300

    MongoDB Connect

---
+++ db.js
@@ -0,0 +1,9 @@
+import dns from "node:dns";
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((error) => console.log(error));
